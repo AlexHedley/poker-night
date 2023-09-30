f9f8df193243470e11d0ef61bae302d5d75d8ac5
Alex Hedley <alex.hedley@me.com>
2023-09-30 13:57:28 +0100

    WIP Wizard steps

--- docs/ACKNOWLEDGEMENTS.md
+++ docs/ACKNOWLEDGEMENTS.md
@@ -4,3 +4,6 @@
 - [Bootstrap](https://getbootstrap.com/)
 - [jQuery](https://jquery.com/)
 - [jQuery UI](https://jqueryui.com/)
+
+- [bootstrap-wizard](https://github.com/jeffreypolk/bootstrap-wizard) code
+  - [bootstrap-wizard](https://jeffreypolk.github.io/bootstrap-wizard/) site
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,22 @@
 var app = angular.module('app', []);
 app.controller('controller', function ($scope, $http, $q, $filter) {
     $scope.init = function () {
+        // initialize wizard
+        $('#wizard').wizard({
+            title: 'Custom Title',
+            onSubmit: function () {
+                $('<div>onSubmit called</div>').appendTo('#EventLog');
+                $('#wizard').wizard('end', {
+                    info: 'this is an info message',
+                    warning: 'this is a warning message',
+                    success: 'this is a success message',
+                    error: 'this is an error message',
+                    // autoClose: 5000 // close after 5 seconds
+                });
+            },
+            isModal: false,
+            autoOpen: true
+        });
     }
 
     $scope.init();
